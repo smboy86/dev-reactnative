87d095ecead94abcfc7c1585ad5620bb8acf04da
smboy86 <smboy86@naver.com>
2019-12-31 00:20:03 +0900

    메시지 테스트

--- firebasetest/App.js
+++ firebasetest/App.js
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,56 +14,102 @@ import {
   View,
   Text,
   StatusBar,
+  Alert,
 } from 'react-native';
-// import firebase from 'react-native-firebase';
+import firebase from 'react-native-firebase';
 
 const App = () => {
+  useEffect(() => {
+    console.log('test useEffect ');
+    this.checkPermission();
+    this.messageListener();
+  }, []);
+
+  checkPermission = async () => {
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      this.getFcmToken();
+    } else {
+      this.requestPermission();
+    }
+  };
+
+  getFcmToken = async () => {
+    const fcmToken = await firebase.messaging().getToken();
+    if (fcmToken) {
+      console.log(fcmToken);
+      this.showAlert('Your Firebase Token is:', fcmToken);
+    } else {
+      this.showAlert('Failed', 'No token received');
+    }
+  };
+
+  messageListener = async () => {
+    this.notificationListener = firebase
+      .notifications()
+      .onNotification(notification => {
+        const {title, body} = notification;
+        this.showAlert(title, body);
+      });
+
+    this.notificationOpenedListener = firebase
+      .notifications()
+      .onNotificationOpened(notificationOpen => {
+        const {title, body} = notificationOpen.notification;
+        this.showAlert(title, body);
+      });
+
+    const notificationOpen = await firebase
+      .notifications()
+      .getInitialNotification();
+    if (notificationOpen) {
+      const {title, body} = notificationOpen.notification;
+      this.showAlert(title, body);
+    }
+
+    this.messageListener = firebase.messaging().onMessage(message => {
+      console.log(JSON.stringify(message));
+    });
+  };
+
+  showAlert = (title, message) => {
+    Alert.alert(
+      title,
+      message,
+      [{text: 'OK', onPress: () => console.log('OK Pressed')}],
+      {cancelable: false},
+    );
+  };
+
+  requestPermission = async () => {
+    try {
+      await firebase.messaging().requestPermission();
+      // User has authorised
+    } catch (error) {
+      // User has rejected permissions
+    }
+  };
+
   return (
     <>
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>파이어베이스 프로젝트 셋팅!</Text>
-        {/* {firebase.admob.nativeModuleExists && (
-          <Text style={styles.module}>admob()</Text>
-        )}
-        {firebase.analytics.nativeModuleExists && (
-          <Text style={styles.module}>analytics()</Text>
-        )}
-        {firebase.auth.nativeModuleExists && (
-          <Text style={styles.module}>auth()</Text>
-        )}
-        {firebase.config.nativeModuleExists && (
-          <Text style={styles.module}>config()</Text>
-        )}
-        {firebase.crashlytics.nativeModuleExists && (
-          <Text style={styles.module}>crashlytics()</Text>
-        )}
-        {firebase.database.nativeModuleExists && (
-          <Text style={styles.module}>database()</Text>
-        )}
-        {firebase.firestore.nativeModuleExists && (
-          <Text style={styles.module}>firestore()</Text>
-        )}
-        {firebase.functions.nativeModuleExists && (
-          <Text style={styles.module}>functions()</Text>
-        )}
-        {firebase.iid.nativeModuleExists && (
-          <Text style={styles.module}>iid()</Text>
-        )}
-        {firebase.links.nativeModuleExists && (
-          <Text style={styles.module}>links()</Text>
-        )}
-        {firebase.messaging.nativeModuleExists && (
-          <Text style={styles.module}>messaging()</Text>
-        )}
+        <Text>파이어베이스 프로젝트 셋팅! 222</Text>
+        {firebase.admob.nativeModuleExists && <Text>admob()</Text>}
+        {firebase.analytics.nativeModuleExists && <Text>analytics()</Text>}
+        {firebase.auth.nativeModuleExists && <Text>auth()</Text>}
+        {firebase.config.nativeModuleExists && <Text>config()</Text>}
+        {firebase.crashlytics.nativeModuleExists && <Text>crashlytics()</Text>}
+        {firebase.database.nativeModuleExists && <Text>database()</Text>}
+        {firebase.firestore.nativeModuleExists && <Text>firestore()</Text>}
+        {firebase.functions.nativeModuleExists && <Text>functions()</Text>}
+        {firebase.iid.nativeModuleExists && <Text>iid()</Text>}
+        {firebase.links.nativeModuleExists && <Text>links()</Text>}
+        {firebase.messaging.nativeModuleExists && <Text>messaging()</Text>}
         {firebase.notifications.nativeModuleExists && (
-          <Text style={styles.module}>notifications()</Text>
+          <Text>notifications()</Text>
         )}
-        {firebase.perf.nativeModuleExists && (
-          <Text style={styles.module}>perf()</Text>
-        )}
-        {firebase.storage.nativeModuleExists && (
-          <Text style={styles.module}>storage()</Text>
-        )} */}
+        {firebase.perf.nativeModuleExists && <Text>perf()</Text>}
+        {firebase.storage.nativeModuleExists && <Text>storage()</Text>}
       </View>
     </>
   );
