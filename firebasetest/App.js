82383ec98937ab6e09a6904e431f46e77b7fe072
smboy86 <smboy86@naver.com>
2019-12-30 16:56:24 +0900

    패키지명 변경 후 중간 저장

--- firebasetest/App.js
+++ firebasetest/App.js
@@ -15,100 +15,58 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
+// import firebase from 'react-native-firebase';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+const App = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>파이어베이스 프로젝트 셋팅!</Text>
+        {/* {firebase.admob.nativeModuleExists && (
+          <Text style={styles.module}>admob()</Text>
+        )}
+        {firebase.analytics.nativeModuleExists && (
+          <Text style={styles.module}>analytics()</Text>
+        )}
+        {firebase.auth.nativeModuleExists && (
+          <Text style={styles.module}>auth()</Text>
+        )}
+        {firebase.config.nativeModuleExists && (
+          <Text style={styles.module}>config()</Text>
+        )}
+        {firebase.crashlytics.nativeModuleExists && (
+          <Text style={styles.module}>crashlytics()</Text>
+        )}
+        {firebase.database.nativeModuleExists && (
+          <Text style={styles.module}>database()</Text>
+        )}
+        {firebase.firestore.nativeModuleExists && (
+          <Text style={styles.module}>firestore()</Text>
+        )}
+        {firebase.functions.nativeModuleExists && (
+          <Text style={styles.module}>functions()</Text>
+        )}
+        {firebase.iid.nativeModuleExists && (
+          <Text style={styles.module}>iid()</Text>
+        )}
+        {firebase.links.nativeModuleExists && (
+          <Text style={styles.module}>links()</Text>
+        )}
+        {firebase.messaging.nativeModuleExists && (
+          <Text style={styles.module}>messaging()</Text>
+        )}
+        {firebase.notifications.nativeModuleExists && (
+          <Text style={styles.module}>notifications()</Text>
+        )}
+        {firebase.perf.nativeModuleExists && (
+          <Text style={styles.module}>perf()</Text>
+        )}
+        {firebase.storage.nativeModuleExists && (
+          <Text style={styles.module}>storage()</Text>
+        )} */}
+      </View>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
