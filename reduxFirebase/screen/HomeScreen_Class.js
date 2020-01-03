49a96a3525800de2bcb06d32c06552b8ca91785d
smboy86 <smboy86@naver.com>
2020-01-04 02:34:47 +0900

    redux Hook 과 connect의 사용법 차이

---
+++ reduxFirebase/screen/HomeScreen_Class.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+import { connect } from 'react-redux';
+
+class HomeScreen extends Component {
+  state = {
+    counter: 0,
+  };
+
+  increase = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  decrease = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <TouchableOpacity onPress={this.props.increase}>
+          <Text>[ 증가 ]</Text>
+        </TouchableOpacity>
+        <Text>{this.props.counter}</Text>
+        <TouchableOpacity onPress={this.props.decrease}>
+          <Text>[ 감소] </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  const { counter } = state.counter;
+  return {
+    counter: counter,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increase: () => dispatch({ type: 'INCREASE' }),
+    decrease: () => dispatch({ type: 'DECREASE' }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
